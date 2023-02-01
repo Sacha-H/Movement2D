c2d0076faa222ce19b561e29c4c1a5480025c545
YohanGdn <92313723+Sacha-H@users.noreply.github.com>
2023-02-01 17:11:26 +0100

    revue des mouvement horizontaux
    encore a travailler

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,15 +10,19 @@ public class Player : MonoBehaviour
 
     float horizontal_value;
     float vertical_value;
-    float current = 50.0f;
-    float target = 300.0f;
+    float current = 110.0f;
+    float target = 500.0f;
     Vector2 ref_velocity = Vector2.zero;
 
-    [SerializeField] float moveSpeed_horizontal = 400f;
-    [SerializeField] bool is_jumping = false;
-    [SerializeField] bool can_jump = false;
+     float moveSpeed_horizontal = 3000f;
+     bool is_jumping = false;
+    bool can_jump = false;
     float jumpForce = 10f;
-    [Range(0, 1)] [SerializeField] float smooth_time = 0.5f;
+    [Range(0, 1)] [SerializeField] float smooth_time = 0.05f;
+
+    float startSpeed = 3000f;
+    float targetSpeed;
+    float timePassed;
 
 
     // Start is called before the first frame update
@@ -26,7 +30,7 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
-        animController = GetComponent<Animator>();
+        //animController = GetComponent<Animator>();
         Debug.Log(Mathf.Lerp(current, target, 0));
     }
 
@@ -35,15 +39,15 @@ public class Player : MonoBehaviour
     {
         horizontal_value = Input.GetAxis("Horizontal");
 
-        animController.SetFloat("speed", Mathf.Abs(horizontal_value));
-        sr.flipX = horizontal_value < 0;
-        animController.SetFloat("fall", rb.velocity.y);
+        //animController.SetFloat("speed", Mathf.Abs(horizontal_value));
+        //sr.flipX = horizontal_value < 0;
+        //animController.SetFloat("fall", rb.velocity.y);
 
         vertical_value = Input.GetAxis("Vertical");
         if (Input.GetButtonDown("Jump") && can_jump)
         {
             is_jumping = true;
-            animController.SetBool("jumping", true);
+            //animController.SetBool("jumping", true);
         }
     }
     void FixedUpdate()
@@ -54,13 +58,22 @@ public class Player : MonoBehaviour
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             can_jump = false;
         }
+
+
+        timePassed += Time.fixedDeltaTime;
+        targetSpeed = startSpeed * Mathf.Pow(0.01f, timePassed);
+        Debug.Log(targetSpeed);
         Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.fixedDeltaTime, rb.velocity.y);
         rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.05f);
+     
+
+        //Debug.Log(rb.velocity);
+       Debug.Log(target_velocity);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         can_jump = true;
         Debug.Log(collision.gameObject.tag);
-        animController.SetBool("jumping", false);
+        //animController.SetBool("jumping", false);
     }
 }
